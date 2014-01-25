b7c2b6a963eb502a367bdc3c66b9ae34f8443a23
Dmitry Polishuk <dp@neosphere.com>
2014-01-25 14:30:34 +0400

    spaces vs tabs

--- eventbus.hpp
+++ eventbus.hpp
@@ -6,39 +6,36 @@
 
 #include <boost/signals2.hpp>
 
-#include "common/events.h"
-
-template <class T>
+template<class T>
 class singleton {
-public:
-  template <typename... Args>
-  static
-  T* get_instance(Args... args) {
-	if (!m_instance) {
-	  m_instance = new T(std::forward<Args>(args)...);
-	}
+ public:
+  template<typename ... Args>
+  static T* get_instance(Args ... args) {
+    if (!m_instance) {
+      m_instance = new T(std::forward<Args>(args)...);
+    }
 
-	return m_instance;
+    return m_instance;
   }
 
   static
   void destroy_instance() {
-	delete m_instance;
-	m_instance = nullptr;
+    delete m_instance;
+    m_instance = nullptr;
   }
 
-private:
+ private:
   static T* m_instance;
 };
 
-template <class T> T*  singleton<T>::m_instance = nullptr;
+template<class T> T* singleton<T>::m_instance = nullptr;
 
 class event {
 
 };
 
 class event_bus : public singleton<event_bus> {
-public:
+ public:
   event_bus() {
   }
 
@@ -46,47 +43,49 @@ public:
   }
 
   template<typename T>
-  boost::signals2::signal<T>* createSignal() {
-	typedef boost::signals2::signal<T> Signal;
+  boost::signals2::signal<T>* create_signal() {
+    typedef boost::signals2::signal<T> signal_t;
 
-	if (m_signals.find (typeid (T).name()) == m_signals.end ()) {
-	  Signal* signal = new Signal();
-	  m_signals[typeid (T).name()] = signal;
-	  return (signal);
-	}
-	return (0);
+    if (m_signals.find(typeid(T).name()) == m_signals.end()) {
+      signal_t* signal = new signal_t();
+      m_signals[typeid(T).name()] = signal;
+      return (signal);
+    }
+
+    return 0;
   }
 
   template<typename T>
   boost::signals2::connection subscribe(const boost::function<T>& callback) {
-	typedef boost::signals2::signal<T> Signal;
-	Signal* signal = nullptr;
+    typedef boost::signals2::signal<T> signal_t;
+    signal_t* signal = nullptr;
 
-	if (m_signals.find(typeid(T).name()) == m_signals.end()) {
-//	  MY_LOGE("no callback for type: %s", typeid (T).name());
-	  signal = createSignal<T>();
-	} else {
-	  signal = dynamic_cast<Signal*> (m_signals[typeid(T).name()]);
-	}
+    if (m_signals.find(typeid(T).name()) == m_signals.end()) {
+      signal = create_signal<T>();
+    } else {
+      signal = dynamic_cast<signal_t*>(m_signals[typeid(T).name()]);
+    }
 
-	boost::signals2::connection ret = signal->connect(callback);
+    boost::signals2::connection ret = signal->connect(callback);
 
-	m_connections[typeid(T).name()].push_back (ret);
-	m_shared_connections[typeid(T).name()].push_back (boost::signals2::shared_connection_block (m_connections[typeid (T).name ()].back(), false));
+    m_connections[typeid(T).name()].push_back(ret);
+    m_shared_connections[typeid(T).name()].push_back(
+        boost::signals2::shared_connection_block(
+            m_connections[typeid(T).name()].back(), false));
 //    signalsChanged ();
-	return (ret);
+    return (ret);
   }
 
-  template <typename T, typename... Args>
-  void post(Args... args) {
-    typedef boost::signals2::signal<T> Signal;
+  template<typename T, typename ... Args>
+  void post(Args ... args) {
+    typedef boost::signals2::signal<T> signal_t;
     if (m_signals.find(typeid(T).name()) != m_signals.end()) {
-      Signal* signal = dynamic_cast<Signal*> (m_signals[typeid(T).name()]);
+      signal_t* signal = dynamic_cast<signal_t*>(m_signals[typeid(T).name()]);
       signal->operator()(std::forward<Args>(args)...);
     }
   }
 
-private:
+ private:
   std::map<std::string, boost::signals2::signal_base*> m_signals;
   std::map<std::string, std::vector<boost::signals2::connection> > m_connections;
   std::map<std::string, std::vector<boost::signals2::shared_connection_block> > m_shared_connections;
--- main.cpp
+++ main.cpp
@@ -7,17 +7,23 @@ using namespace std;
 void f(int i);
 
 void test(int i) {
-	std::cout << "hello from test\n";
+  std::cout << "hello from test\n";
 }
 
-int main()
-{
-	event_bus bus;
-	boost::function<void (int)> f = &test;
-	bus.subscribe(f);
+class my_event {
 
-	bus.post<void (int)>(1);
+};
 
-	return 0;
+void my_event_callback(my_event e) {
+  std::cout << "Got event!\n";
 }
 
+int main() {
+  event_bus bus;
+  boost::function<void(int)> f = &test;
+  bus.subscribe(f);
+
+  bus.post<void(int)>(1);
+
+  return 0;
+}
